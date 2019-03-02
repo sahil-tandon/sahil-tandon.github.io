4fc942f6f240b9bf9b36d2b1fc5c2ed1b138a5f7
Sahil Tandon <sahil.tandon@live.com>
2019-03-02 03:59:22 -0500

    Added BroswserSync and updated gulpfile.js.

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,30 @@
    ----------------------------- */
 
 var gulp = require('gulp'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync').create();
 
 /* ==[ Convert Sass to CSS ]== */
-gulp.task('sass', function(){
+gulp.task('sass', function() {
   return gulp.src('app/scss/*.scss')
     .pipe(sass())     
     .pipe(gulp.dest('app/css'));
 });
 
-/* ==[ Watch for Changes ]== */
-gulp.task('watch', function(){
-  gulp.watch('app/scss/*.scss', gulp.series('sass'));
+/* ==[ Live-reload on Browser ]== */
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: './app'
+    },
+  });
 });
+
+/* ==[ Watch for Changes ]== */
+gulp.task('watch', ['browserSync', 'sass'], function() {
+  gulp.watch('app/scss/*.scss', ['sass']);
+  gulp.watch('*.html', browserSync.reload);
+  gulp.watch('app/css/*.css', browserSync.reload);
+  gulp.watch('app/js/*.js', browserSync.reload);
+});
+
