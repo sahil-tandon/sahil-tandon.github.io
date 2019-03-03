3e92ac1a261f41e78e59f84345305f7ca3da128d
Sahil Tandon <sahil.tandon@live.com>
2019-03-03 00:32:34 -0500

    Changes to setup gulp environment.

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,16 @@
 
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    browserSync = require('browser-sync').create();
+    browserSync = require('browser-sync').create(),
+    useref = require('gulp-useref'),
+    uglify = require('gulp-uglify'),
+    gulpIf = require('gulp-if'),
+    cssnano = require('gulp-cssnano');
 
 /* ==[ Convert Sass to CSS ]== */
 gulp.task('sass', function() {
   return gulp.src('app/scss/*.scss')
-    .pipe(sass())     
+    .pipe(sass())
     .pipe(gulp.dest('app/css'));
 });
 
@@ -16,9 +20,18 @@ gulp.task('sass', function() {
 gulp.task('browserSync', function() {
   browserSync.init({
     server: {
-      baseDir: './app'
+      baseDir: 'app'
     },
   });
+});
+
+/* ==[ Concatenate specified files in HTML ]== */
+gulp.task('useref', function() {
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))     //minify JS files
+    .pipe(gulpIf('*.css', cssnano()))     //minify CSS files
+    .pipe(gulp.dest('dist'))
 });
 
 /* ==[ Watch for Changes ]== */
@@ -28,4 +41,3 @@ gulp.task('watch', ['browserSync', 'sass'], function() {
   gulp.watch('app/css/*.css', browserSync.reload);
   gulp.watch('app/js/*.js', browserSync.reload);
 });
-
