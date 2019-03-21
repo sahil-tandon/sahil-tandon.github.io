2b79eff6e54ea60cdf707551311231736c129854
Sahil Tandon <sahil.tandon@live.com>
2019-03-20 22:26:50 -0400

    Added script to handle hamburger icon animation.

--- app/js/app.js
+++ app/js/app.js
@@ -1,2 +1,23 @@
 /* app.js
    ----------------------------- */
+
+   var app = {
+   		init : function(){
+   			app.initCache();
+   			app.initEvents();
+   		},
+   		initCache : function(){
+   			app.cache = {
+   				hamburger : $('.hamburger')
+   			}
+   		},
+   		initEvents : function(){
+   			app.cache.hamburger.click(function(){
+   				$(this).toggleClass('active');
+   			});
+   		},
+   };
+
+   $(document).ready(function(){
+   		app.init();
+   });
