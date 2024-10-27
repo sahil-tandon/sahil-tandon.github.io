6ffdb33f025e7df80bacf58a6acdef88950fc3fd
Sahil Tandon <sahil.tandon@live.com>
2024-10-27 20:19:39 +0530

    Replace local Geist fonts with Oxanium from Google Fonts.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        oxanium: ['var(--font-oxanium)', 'sans-serif'],
+      },
     },
   },
   plugins: [],
